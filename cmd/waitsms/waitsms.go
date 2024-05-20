a2c6b5d7a7101f3609b25ba1d422555b566e7cbd
Merdan <merdan.tmbiz@gmail.com>
2024-05-20 21:17:13 +0500

    sms grpc server

--- cmd/waitsms/waitsms.go
+++ cmd/waitsms/waitsms.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"log"
+	"net"
 
+	pb "github.com/warthog618/modem/gen"
 	"github.com/warthog618/modem/pkg/config"
+	rpc "github.com/warthog618/modem/pkg/grpc"
 	"github.com/warthog618/modem/pkg/sms"
+	"google.golang.org/grpc"
 )
-
-var version = "undefined"
 
 func main() {
 
@@ -17,9 +19,27 @@ func main() {
 		log.Println(err)
 	}
 
-	err = sms.StartSMSReciever(&config.TomlConf)
+	//start grpc stream
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterExampleServiceServer(s, &rpc.Server{
+		Clients: make(map[string]pb.ExampleService_StreamDataServer),
+	})
+
+	log.Println("Server is running on port :50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+	//start sms reciver service
+	err = sms.StartSMSReciever(&config.TomlConf, &s)
 
 	if err != nil {
 		log.Println(err)
 	}
+
 }
